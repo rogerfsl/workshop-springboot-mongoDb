8965abbfbf4194e0cfdb1ac1352f092c5468bd7c
Rogerfsl <rogerfsl@hotmail.com>
2024-02-21 18:34:13 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/rogerlopes/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.rogerlopes.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rogerlopes.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+
+}
