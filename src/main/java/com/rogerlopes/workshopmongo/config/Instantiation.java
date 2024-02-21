536910a31b370d8da9a937a5dfcf2452efebc994
Rogerfsl <rogerfsl@hotmail.com>
2024-02-21 11:09:26 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/rogerlopes/workshopmongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.rogerlopes.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.rogerlopes.workshopmongo.domain.User;
+import com.rogerlopes.workshopmongo.repository.UserRepository;
+
+
+@Configuration
+public class Instantiation  implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository repository;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
